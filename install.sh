1630dbaef7ef93d9867a0e3a4ef80d29875bb2f5
Sven Schmit <spsnld@stanford.edu>
2015-09-24 22:10:54 -0700

    updated install to manually install zsh for safety

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Installing config files in home directory"
 
-cp zshrc ~/.zshrc
+echo "Please manually install zshrc"
+# cp zshrc ~/.zshrc
+
 cp vimrc ~/.vimrc
 cp ackrc ~/.ackrc
 cp tmux.conf ~/.tmux.conf
