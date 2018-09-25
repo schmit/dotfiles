4cd880b8da6b1635bf5ad2515321c1269e637cde
Sven Schmit <schmit@stanford.edu>
2018-09-24 22:15:25 -0700

    remove vimrc from install

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ echo "Installing config files in home directory"
 echo "Please manually install zshrc"
 # cp zshrc ~/.zshrc
 
-cp vimrc ~/.vimrc
-
 # note nvim config file is not nvimrc
 cp nvimrc ~/.config/nvim/init.vim
 cp ackrc ~/.ackrc
