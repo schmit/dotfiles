eb0e8157556e67ecedcb4ebfec4bc222d31872b9
Sven Schmit <sven@geteppo.com>
2025-09-07 14:40:17 -0700

    Add ghostty config (#8)

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "Installing config files in home directory"
 
 # note nvim config file is not nvimrc
 cp -R nvim ~/.config/
+cp -R ghostty ~/.config/
 cp tmux.conf ~/.tmux.conf
 cp emacs ~/.emacs
 
